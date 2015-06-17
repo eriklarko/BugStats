da4bc113935c1ca46a3bbf79e244c7771c17e4fa
Erik Larkö <erik@larko.se>
2015-06-17 20:56:40 +0200

    Can list files modified in commit

--- src/main.go
+++ src/main.go
@@ -19,6 +19,9 @@ func main() {
 	for _, commit := range(gitLog) {
 		if isBugFixCommit(&commit) {
 			fmt.Printf("%s indicates a bugfix\n", commit.Message)
+
+			fmt.Printf("%v\n", getModifiedFiles(commit.Hash))
+			return
 		}
 	}
 }
@@ -42,4 +45,15 @@ func getLog() []asd.HashAndMessage {
 func isBugFixCommit(commit *asd.HashAndMessage) bool {
 	message := strings.ToLower(commit.Message)
 	return strings.Contains(message, "fixes") || strings.Contains(message, "bugfix") || strings.Contains(message, "bug-fix")
+}
+
+func getModifiedFiles(commitHash string) []string {
+	// TODO: How does the git diff --name-only output look for moved files?
+	// TODO: Does this work for the first commit?
+
+	rawOutput, err := session.Command("git", "diff", "--name-only", commitHash, commitHash + "^").Output()
+	if err != nil {
+		log.Panicf("Unable to get list of files modified in %s. %v\n", commitHash, err)
+	}
+	return strings.Split(string(rawOutput), "\n")
 }
